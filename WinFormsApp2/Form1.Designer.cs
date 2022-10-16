4685e3d100e1bda1ee672c5a36ab7fefd6832b95
Khang0202 <nguyenmanhkhang2002@gmail.com>
2022-10-16 14:11:43 +0700

    them giao dien va code xu ly

--- WinFormsApp2/Form1.Designer.cs
+++ WinFormsApp2/Form1.Designer.cs
@@ -28,12 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtKetQua = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btCong = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Số a: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Số b: ";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(71, 25);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(136, 26);
+            this.txtA.TabIndex = 2;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(71, 72);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(136, 26);
+            this.txtB.TabIndex = 3;
+            // 
+            // txtKetQua
+            // 
+            this.txtKetQua.Location = new System.Drawing.Point(92, 119);
+            this.txtKetQua.Name = "txtKetQua";
+            this.txtKetQua.Size = new System.Drawing.Size(115, 26);
+            this.txtKetQua.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 19);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Kết quả: ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // btCong
+            // 
+            this.btCong.Location = new System.Drawing.Point(71, 170);
+            this.btCong.Name = "btCong";
+            this.btCong.Size = new System.Drawing.Size(86, 26);
+            this.btCong.TabIndex = 6;
+            this.btCong.Text = "+";
+            this.btCong.UseVisualStyleBackColor = true;
+            this.btCong.Click += new System.EventHandler(this.btCong_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(242, 212);
+            this.Controls.Add(this.btCong);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtKetQua);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtA;
+        private TextBox txtB;
+        private TextBox txtKetQua;
+        private Label label3;
+        private Button btCong;
     }
 }
